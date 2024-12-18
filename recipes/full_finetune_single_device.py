8d72d2770aa9c194c18adf86a5d5d4ba6715a705
Sahar Shayegan <saharom.omidishayegan@servicenow.com>
2024-12-18 21:36:50 +0000

    interactive reply with python calls is working

--- recipes/full_finetune_single_device.py
+++ recipes/full_finetune_single_device.py
@@ -16,7 +16,7 @@ from omegaconf import DictConfig, ListConfig
 
 from torch import nn
 from torch.optim import Optimizer
-from torch.utils.data import DataLoader, DistributedSampler
+from torch.utils.data import DataLoader, DistributedSampler, Subset
 
 from torchtune import config, modules, training, utils
 from torchtune.config._utils import _get_component_from_path
@@ -26,6 +26,7 @@ from torchtune.recipe_interfaces import FTRecipeInterface
 from torchtune.training import DummyProfiler, PROFILER_KEY
 from torchtune.training.lr_schedulers import get_lr
 
+import random
 from tqdm import tqdm
 
 
@@ -322,13 +323,40 @@ class FullFinetuneRecipeSingleDevice(FTRecipeInterface):
         # NOTE: added by us
         # validation dataloader
         cfg["validation_dataset"] = deepcopy(cfg.dataset)
-        cfg["validation_dataset"]["split"] = "validation"
-        self._sampler_validation, self._dataloader_validation = self._setup_data(
-            cfg_dataset=cfg["validation_dataset"],
-            shuffle=cfg.shuffle,
-            batch_size=cfg.batch_size,  # TODO: have a separate batch size for validation
-            collate_fn=collate_name,
-        )
+        self._sampler_validation_list = []
+        self._dataloader_validation_list = []
+        if isinstance(cfg["validation_dataset"]["_component_"], ListConfig):
+            portions = [dataset['portion'] for dataset in cfg["validation_dataset"]["_component_"]]
+            for dataset in cfg["validation_dataset"]["_component_"]:
+                dataset["split"] = "validation"
+                sampler_validation, dataloader_validation = self._setup_data(
+                    cfg_dataset=dataset,
+                    shuffle=cfg.shuffle,
+                    batch_size=cfg.batch_size,
+                    collate_fn=collate_name,
+                )
+                self._sampler_validation_list.append(sampler_validation)
+                self._dataloader_validation_list.append(dataloader_validation)
+            # downsample the validation using the portion property in the dataset config
+            for i in range(1,len(self._sampler_validation_list)): # hardcoding the first portion to be the training portion
+                sample_size = int(len(self._dataloader_validation_list[0].dataset) * portions[i] / (1-sum(portions[1:]))) # hardcoding the first portion to be the training portion
+                if sample_size > len(self._dataloader_validation_list[i].dataset):
+                    continue
+                random.seed(42)
+                subset = Subset(self._dataloader_validation_list[i].dataset, random.sample(range(len(self._dataloader_validation_list[i].dataset)), sample_size))
+                collate_fn = _get_component_from_path(collate_name)
+                self._dataloader_validation_list[i] = DataLoader(subset, batch_size=cfg.batch_size,shuffle=False, collate_fn=collate_fn)
+        else:
+            print("WARNING: Validation dataset is not a list.")
+            cfg["validation_dataset"]["split"] = "validation"
+            sampler_validation, dataloader_validation = self._setup_data(
+                cfg_dataset=cfg["validation_dataset"],
+                shuffle=cfg.shuffle,
+                batch_size=cfg.batch_size,  # TODO: have a separate batch size for validation
+                collate_fn=collate_name,
+            )
+            self._sampler_validation_list.append(sampler_validation)
+            self._dataloader_validation_list.append(dataloader_validation)
 
         # Finally update the recipe state which can only be correctly set after all of the
         # other components have been initialized and updated.
@@ -579,16 +607,81 @@ class FullFinetuneRecipeSingleDevice(FTRecipeInterface):
         DistributedSamplers with Map-style Datasets which fit into memory. Other samplers,
         iterable datasets and streaming datasets are not supported.
         """
-        if isinstance(cfg_dataset, ListConfig):
-            datasets = [
-                config.instantiate(single_cfg_dataset, self._tokenizer)
-                for single_cfg_dataset in cfg_dataset
-            ]
-            ds = ConcatDataset(datasets=datasets)
+
+        def update_component_key(obj):
+            """
+            Update the key '._component_' to '_component_' in the dictionary.
+            """
+            if "._component_" in obj:
+                obj["_component_"] = obj.pop("._component_")
+
+            for key, value in obj.items():
+                if isinstance(value, dict):
+                    update_component_key(value)
+
+            return obj
+
+        def convert_to_nested(config):
+            """
+            Converts a flattened dictionary (e.g., 'column_map.output')
+            into a nested dictionary structure.
+
+            Args:
+                config (dict): A dictionary with flattened keys.
+
+            Returns:
+                dict: A dictionary with nested keys.
+            """
+            # print('cfg in the convert_to_nested:', config)
+            nested_config = {}
+
+            for key, value in config.items():
+                parts = key.split(".")  # Split the keys by '.'
+                current = nested_config
+
+                for part in parts[:-1]:
+                    if part not in current:
+                        current[part] = {}
+                    current = current[part]
+
+                current[parts[-1]] = value
+
+            return nested_config
+
+        # print('cfg begining of the setup data:', cfg_dataset)
+        if isinstance(cfg_dataset.get("_component_", None), ListConfig):
+            portions = []
+            datasets = []
+            for single_cfg_dataset in cfg_dataset["_component_"]:
+                single_cfg_dataset = DictConfig(
+                    convert_to_nested(
+                        update_component_key(deepcopy(single_cfg_dataset))
+                    )
+                )
+
+                portion = (
+                    single_cfg_dataset.pop("portion")
+                    if "portion" in single_cfg_dataset
+                    else None
+                )
+                portions.append(portion)
+                print('cfg in the list of setup data:', single_cfg_dataset)
+                datasets.append(
+                    config.instantiate(
+                        single_cfg_dataset,
+                        self._tokenizer,
+                    )
+                )
+            # print('Portions is:', portions)
+            ds = ConcatDataset(datasets=datasets, portions=portions)
             packed = False
         else:
-            ds = config.instantiate(cfg_dataset, self._tokenizer)
-            packed = cfg_dataset.get("packed", False)
+            new_single_cfg_dataset = DictConfig(
+                convert_to_nested(update_component_key(deepcopy(cfg_dataset)))
+            )
+            new_single_cfg_dataset.pop("portion", None)
+            ds = config.instantiate(new_single_cfg_dataset, self._tokenizer)
+            packed = new_single_cfg_dataset.get("packed", False)
 
         # Instantiate collate_fn
         if "left_pad_sequence" in collate_fn:
@@ -720,62 +813,66 @@ class FullFinetuneRecipeSingleDevice(FTRecipeInterface):
             # Update the sampler to ensure data is correctly shuffled across epochs
             # in case shuffle is True
             self._sampler.set_epoch(curr_epoch)
-            self._sampler_validation.set_epoch(curr_epoch)  # NOTE: added by us
+            for _sampler_validation in self._sampler_validation_list:
+                _sampler_validation.set_epoch(curr_epoch)
 
             # NOTE: added by us
             # ------ Validation Step ------ #
             self._model.eval()
 
             with torch.no_grad():
-                cum_val_loss = 0
-                num_eval_steps = (
-                    min(self._max_validation_steps, len(self._dataloader_validation))
-                    if self._max_validation_steps is not None
-                    else len(self._dataloader_validation)
-                )
-                # NOTE: added by us
-                # start a counter for samples that are too long
-                max_len_samples = 0
-
-                pbar_val = tqdm(total=num_eval_steps, desc="Validation")
-                # NOTE: added by us - counter to account for samples that are too long
-                idx = 0
-                for _, batch in enumerate(self._dataloader_validation):
-
-                    # NOTE: added by us
-                    if self._skip_max_seq_len_samples(batch):
-                        max_len_samples += 1
-                        continue
-
-                    utils.batch_to_device(batch, self._device)
-                    # TODO: finish this feature
-                    # try:
-                    val_loss = self._loss_step(batch)
-                    # except RuntimeError as e:
-                    #     log.error(f"Error in validation loss computation: {e}")
-                    #     val_loss = torch.tensor(0.0, device=self._device)
-                    #     continue
-                    cum_val_loss += val_loss
-                    pbar_val.update(1)
-                    pbar_val.set_description(
-                        f"{self.epochs_run+1}|{self.global_step}|Validation Loss: {cum_val_loss / (idx + 1)}"
+                for i, dataloader_validation in enumerate(
+                    self._dataloader_validation_list
+                ):
+                    cum_val_loss = 0
+                    num_eval_steps = (
+                        min(self._max_validation_steps, len(dataloader_validation))
+                        if self._max_validation_steps is not None
+                        else len(dataloader_validation)
                     )
-                    idx += 1
+                    # NOTE: added by us
+                    # start a counter for samples that are too long
+                    max_len_samples = 0
 
-                    if (
-                        self._max_validation_steps is not None
-                        and idx == self._max_validation_steps
-                    ):
-                        break
+                    pbar_val = tqdm(total=num_eval_steps, desc="Validation")
+                    # NOTE: added by us - counter to account for samples that are too long
+                    idx = 0
+                    for _, batch in enumerate(dataloader_validation):
 
-                mean_val_loss = cum_val_loss / (idx + 1 - max_len_samples)
-                self._metric_logger.log_dict(
-                    {"val_loss": mean_val_loss},
-                    step=self.global_step,
-                )
-                pbar_val.close()
-                print("Number of samples that were too long: ", max_len_samples)
+                        # NOTE: added by us
+                        if self._skip_max_seq_len_samples(batch):
+                            max_len_samples += 1
+                            continue
 
+                        utils.batch_to_device(batch, self._device)
+                        # TODO: finish this feature
+                        # try:
+                        val_loss = self._loss_step(batch)
+                        # except RuntimeError as e:
+                        #     log.error(f"Error in validation loss computation: {e}")
+                        #     val_loss = torch.tensor(0.0, device=self._device)
+                        #     continue
+                        cum_val_loss += val_loss
+                        pbar_val.update(1)
+                        pbar_val.set_description(
+                            f"{self.epochs_run+1}|{self.global_step}|Validation Loss: {cum_val_loss / (idx + 1)}"
+                        )
+                        idx += 1
+
+                        if (
+                            self._max_validation_steps is not None
+                            and idx == self._max_validation_steps
+                        ):
+                            break
+
+                    mean_val_loss = cum_val_loss / (idx + 1 - max_len_samples)
+                    self._metric_logger.log_dict(
+                        {"val_loss": mean_val_loss},
+                        step=self.global_step,
+                    )
+                    pbar_val.close()
+                    print("Number of samples that were too long: ", max_len_samples)
+            # -------------------- CHANGE ABOVE FOR VALIDATION MUTIPLE DATA LOADERS ---------------------#
             # ------ Training Epoch ------ #
             # Initialize tokens count and running loss (for grad accumulation)
             t0 = time.perf_counter()
@@ -930,6 +1027,7 @@ def recipe_main(cfg: DictConfig) -> None:
         - Parameters specified in config (see available configs through ``tune ls``)
         - Overwritten by arguments from the command-line
     """
+    # print('Cfg in recipe_main:', cfg)
     config.log_config(recipe_name="FullFinetuneRecipeSingleDevice", cfg=cfg)
     recipe = FullFinetuneRecipeSingleDevice(cfg=cfg)
     recipe.setup(cfg=cfg)
@@ -938,4 +1036,5 @@ def recipe_main(cfg: DictConfig) -> None:
 
 
 if __name__ == "__main__":
+    # print('In full finetune recipe')
     sys.exit(recipe_main())
